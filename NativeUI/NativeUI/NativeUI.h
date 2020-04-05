dcaa027f58aee955f3945373198bdc8b7822bd74
Anton Poltoratskyi <a.poltoratskyi@andersenlab.com>
2020-04-05 15:02:31 +0300

    Create empty project

---
+++ NativeUI/NativeUI/NativeUI.h
@@ -0,0 +1,19 @@
+//
+//  NativeUI.h
+//  NativeUI
+//
+//  Created by Anton Poltoratskyi on 05.04.2020.
+//  Copyright © 2020 Anton Poltoratskyi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NativeUI.
+FOUNDATION_EXPORT double NativeUIVersionNumber;
+
+//! Project version string for NativeUI.
+FOUNDATION_EXPORT const unsigned char NativeUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NativeUI/PublicHeader.h>
+
+
