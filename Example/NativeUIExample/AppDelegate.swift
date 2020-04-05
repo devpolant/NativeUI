737ddef87bfbba1fdbed6417d15c3d5e7b36aa29
Anton Poltoratskyi <a.poltoratskyi@andersenlab.com>
2020-04-05 17:16:56 +0300

    Added example project workspace

---
+++ Example/NativeUIExample/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  NativeUIExample
+//
+//  Created by Anton Poltoratskyi on 05.04.2020.
+//  Copyright © 2020 Anton Poltoratskyi. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ Example/NativeUIExample/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  NativeUIExample
+//
+//  Created by Anton Poltoratskyi on 05.04.2020.
+//  Copyright © 2020 Anton Poltoratskyi. All rights reserved.
+//
+
+import UIKit
+import NativeUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
